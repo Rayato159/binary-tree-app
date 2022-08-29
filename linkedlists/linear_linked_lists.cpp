b0dc0af54e0f9b263b6dd993c3b8d4255abecc26
Rayato159 <rayato159@gmail.com>
2022-08-29 20:56:51 +0700

    -feature :sparkles: :Count, sum and max node was added

--- linkedlists/linear_linked_lists.cpp
+++ linkedlists/linear_linked_lists.cpp
@@ -18,6 +18,9 @@ class LinkedLists {
         // Methods
         Node* get_head();
         Node* add_node(Node *n, int data);
+        int node_count();
+        int sum_node();
+        int max_node();
         void display();
 };
 
@@ -31,12 +34,47 @@ Node *LinkedLists::get_head() {
     return this->head;
 }
 
+// O(1)
 Node *LinkedLists::add_node(Node *last, int data) {
     Node *t = new Node;
     t->data = data;
     t->next = NULL;
     last->next = t;
     return t;
+}
+
+// O(n)
+int LinkedLists::node_count() {
+    Node *p = this->head;
+    int count = 0;
+    while(p) {
+        count++;
+        p = p->next;
+    }
+    return count;
+}
+
+int LinkedLists::sum_node() {
+    Node *p = this->head;
+    int sum = 0;
+    while(p) {
+        sum += p->data;
+        p = p->next;
+    }
+    return sum;
+}
+
+int LinkedLists::max_node() {
+    Node *p = this->head;
+    int max = p->data;
+    p = p->next;
+    while(p) {
+        if(max < p->data) {
+            max = p->data;
+        }
+        p = p->next;
+    }
+    return max;
 }
 
 void LinkedLists::display() {
@@ -54,11 +92,14 @@ int main() {
     Node* last = head;
 
     last = l.add_node(last, 20);
+    last = l.add_node(last, 50);
     last = l.add_node(last, 30);
     last = l.add_node(last, 40);
-    last = l.add_node(last, 50);
 
     l.display();
+    cout << "count:\t" << l.node_count() << endl;
+    cout << "sum:\t" << l.sum_node() << endl;
+    cout << "max:\t" << l.max_node() << endl;
 
     return 0;
 }

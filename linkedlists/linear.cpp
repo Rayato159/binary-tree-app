24649b581f54fcd52e47cde4a353ae89c9f94014
Rayato159 <rayato159@gmail.com>
2022-08-31 21:52:05 +0700

    -feature :sparkles: :Linear linked lists was completed

--- linkedlists/linear.cpp
+++ linkedlists/linear.cpp
@@ -19,6 +19,9 @@ class LinkedLists {
         int length();
         int sum_node();
         int search(int key);
+        void insert_n(int data, int pos);
+        void delete_n(int pos);
+        void delete_all();
         void display();
 };
 
@@ -80,6 +83,61 @@ int LinkedLists::search(int key) {
     return -1;
 }
 
+void LinkedLists::insert_n(int data, int pos) {
+    Node *t = new Node;
+    t->data = data;
+    t->next = NULL;
+    Node *p = this->head;
+
+    if(pos == 0) {
+        t->next = this->head;
+        this->head = t;
+    } else if(pos == this->length()-1) {
+        this->insert_last(data);
+    } else {
+        for(int i=0; i<pos-1; i++) {
+            p = p->next;
+        }
+        t->next = p->next;
+        p->next = t;
+        p = t;
+    }
+}
+
+void LinkedLists::delete_n(int pos) {
+    Node *p = this->head;
+    Node *q = NULL;
+
+    if(pos == 0) {
+        this->head = this->head->next;
+        delete p;
+    } else if(pos == this->length()-1) {
+        for(int i=0; i<pos; i++) {
+            q = p;
+            p = p->next;
+        }
+        q->next = NULL;
+        this->last = q;
+    } else {
+        for(int i=0; i<pos; i++) {
+            q = p;
+            p = p->next;
+        }
+        q->next = p->next;
+        delete p;
+    }
+}
+
+void LinkedLists::delete_all() {
+    Node *p = head;
+    while(this->head) {
+        this->head = this->head->next;
+        delete p;
+        p = this->head;
+    }
+    cout << "all nodes are deleted" << endl;
+}
+
 void LinkedLists::display() {
     Node *p = this->head;
     while(p) {
@@ -97,9 +155,10 @@ int main() {
     l.insert_last(40);
     l.insert_last(50);
     l.display();
-    cout << l.length() << endl;
-    cout << l.sum_node() << endl;
-    cout << "key " << l.search(5) << endl;
+    // cout << l.length() << endl;
+    // cout << l.sum_node() << endl;
+    // cout << "key " << l.search(40) << endl;
+    l.delete_all();
     l.display();
     return 0;
 }

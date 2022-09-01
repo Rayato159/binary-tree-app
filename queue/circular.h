a0cdd26bb9d3736b6635fcc069960d0f2c646c7b
Rayato159 <rayato159@gmail.com>
2022-09-01 23:54:03 +0700

    -fix :bug: :Queue was fixed

--- queue/circular.h
+++ queue/circular.h
@@ -25,32 +25,26 @@ class Queue {
 };
 
 Queue::Queue(int size) {
-    this->data = new int[size+1]{0};
+    this->data = new int[size + 1]{0};
     this->size = size + 1;
-    this->front = 0;
-    this->rear = 0;
+    this->front = 1;
+    this->rear = 1;
 }
 
 bool Queue::is_full() {
-    if(this->rear % this->size + 1 == this->front) {
-        return true;
-    }
-    return false;
+    return this->rear % this->size + 1 == this->front;
 }
 
 bool Queue::is_empty() {
-    if(this->front == this->rear) {
-        return true;
-    }
-    return false;
+    return this->front == this->rear;
 }
 
 void Queue::enqueue(int x) {
     if(this->is_full()) {
         std::cout << "error, queue is full" << std::endl;
     } else {
-        this->rear = this->rear % this->size + 1;
         this->data[this->rear] = x;
+        this->rear = this->rear % this->size + 1;
     }
 }
 
@@ -61,7 +55,7 @@ int Queue::dequeue() {
     } else {
         x = this->data[this->front];
         this->data[this->front] = 0;
-        this->front%this->size + 1;
+        this->front = this->front % this->size + 1;
     }
     return x;
 }
--- trees/binary_tree.cpp
+++ trees/binary_tree.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+
+#include "../queue/circular.h"
+
 using namespace std;
 
 int main() {
+    Queue q(3);
+    q.enqueue(10);
+    q.enqueue(20);
+    q.enqueue(30);
+    q.display();
     return 0;
 }

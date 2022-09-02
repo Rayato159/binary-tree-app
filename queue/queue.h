5868f463d2939f9f5bc5177abd480c492ce75eb6
Rayato159 <rayato159@gmail.com>
2022-09-03 00:20:24 +0700

    -feature :sparkles: :Binary tree was completed

--- queue/queue.h
+++ queue/queue.h
@@ -12,7 +12,7 @@ class Node {
 
 class Queue {
     private:
-        Node **data;
+        Node **Q;
         int size;
         int front;
         int rear;
@@ -30,7 +30,7 @@ class Queue {
 };
 
 Queue::Queue() {
-    this->data = new Node*[1001];
+    this->Q = new Node*[1001];
     this->size = 1001;
     this->front = 1;
     this->rear = 1;
@@ -38,12 +38,12 @@ Queue::Queue() {
     Node *t = new Node;
     t->data = 0;
     t->left = t->right = NULL;
-    this->data[0] = t;
+    this->Q[0] = t;
 }
 
 void Queue::enqueue(Node *data) {
     if(!this->is_full()) {
-        this->data[this->rear] = data;
+        this->Q[this->rear] = data;
         this->rear = this->rear % this->size + 1;
     }
 }
@@ -51,7 +51,7 @@ void Queue::enqueue(Node *data) {
 Node *Queue::dequeue() {
     Node *t;
     if(!this->is_empty()) {
-        t = this->data[this->front];
+        t = this->Q[this->front];
         this->front = this->front % this->size + 1;
     }
     return t;

7d059492ca72e6a155fb01b4dc89cf78f88018c4
Rayato159 <rayato159@gmail.com>
2022-09-02 23:57:27 +0700

    -feature :sparkles: :Binary tree was completed

--- trees/binary_tree.cpp
+++ trees/binary_tree.cpp
@@ -1,31 +1,114 @@
 #include <iostream>
 
-#include "../linkedlists/double.h"
+#include "../queue/queue.h"
 
 using namespace std;
 
 class BinaryTree {
     private:
         Node *root;
+        Queue q;
 
-    // Constructor
-    BinaryTree(){};
+    public:
+        BinaryTree();
 
-    // Methods
-    void plant();
+        void create_tree();
+        Node *get_root();
+        void preorder(Node *p);
+        void inorder(Node *p);
+        void postorder(Node *p);
 };
 
-BinaryTree::BinaryTree() {}
+BinaryTree::BinaryTree() {
+    Queue t(100);
+    this->q = t;
+}
 
-void BinaryTree::plant() {
+void BinaryTree::create_tree() {
     Node *p, *t;
-    int x;
-    cin >> x;
     this->root = new Node;
+    this->root->left = this->root->right = NULL;
+
+    int x;
+    cout << "root: " << flush;
+    cin >> x;
+
     this->root->data = x;
-    this->root->next = this->root->prev = NULL;
+    this->q.enqueue(this->root);
+
+    while(!q.is_empty()) {
+        p = this->q.dequeue();
+        printf("p: %p\n", p);
+        printf("left child of %d: ", p->data);
+        cin >> x;
+
+        if(x != -1) {
+            t = new Node;
+            t->data = x;
+            t->left = t->right = NULL;
+            p->left = t;
+            printf("t: %p\n", t);
+            this->q.enqueue(t);
+        }
+
+        printf("right child of %d: ", p->data);
+        cin >> x;
+
+        if(x != -1) {
+            t = new Node;
+            t->data = x;
+            t->left = t->right = NULL;
+            p->right = t;
+            printf("t: %p\n", t);
+            this->q.enqueue(t);
+        }
+        cout << endl;
+    }
+}
+
+Node *BinaryTree::get_root() {
+    return this->root;
+}
+
+void BinaryTree::preorder(Node *p) {
+    if(p) {
+        cout << p->data << " " << flush;
+        this->preorder(p->left);
+        this->preorder(p->right);
+    }
+}
+
+void BinaryTree::inorder(Node *p) {
+    if(p) {
+        this->inorder(p->left);
+        cout << p->data << " " << flush;
+        this->inorder(p->right);
+    }
+}
+
+void BinaryTree::postorder(Node *p) {
+    if(p) {
+        this->postorder(p->left);
+        this->postorder(p->right);
+        cout << p->data << " " << flush;
+    }
 }
 
 int main() {
+    BinaryTree bt;
+    bt.create_tree();
+    Node *t = bt.get_root();
+
+    cout << "preorder:" << endl;
+    bt.preorder(t);
+    cout << endl;
+
+    cout << "inorder:" << endl;
+    bt.inorder(t);
+    cout << endl;
+
+    cout << "postorder:" << endl;
+    bt.postorder(t);
+    cout << endl;
     return 0;
 }

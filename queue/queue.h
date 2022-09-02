92d57d3c5c37d1a9ed0e05d73b9c1d3cfe8a0c53
Rayato159 <rayato159@gmail.com>
2022-09-02 23:59:47 +0700

    -feature :sparkles: :Binary tree was completed

--- queue/queue.h
+++ queue/queue.h
@@ -6,13 +6,13 @@
 class Node {
     public:
         int data;
-        Node *next;
-        Node *prev;
+        Node *left;
+        Node *right;
 };
 
 class Queue {
     private:
-        Node *data;
+        Node **data;
         int size;
         int front;
         int rear;
@@ -31,41 +31,36 @@ class Queue {
 };
 
 Queue::Queue(int size) {
-    this->data = new Node[size + 1];
+    this->data = new Node*[size + 1];
     this->size = size + 1;
     this->front = 1;
     this->rear = 1;
 
     Node *t = new Node;
     t->data = 0;
-    t->next = NULL;
-    t->prev = NULL;
-    this->data[0] = *t;
+    t->left = t->right = NULL;
+    this->data[0] = t;
 }
 
 void Queue::enqueue(Node *data) {
     if(!this->is_full()) {
-        this->data[this->rear] = *data;
-        this->rear % this->size + 1;
+        this->data[this->rear] = data;
+        this->rear = this->rear % this->size + 1;
     }
 }
 
 Node *Queue::dequeue() {
-    Node *t = new Node;
-    t->data = 0;
-    t->next = NULL;
-    t->prev = NULL;
+    Node *t;
     if(!this->is_empty()) {
-        *t = this->data[this->front];
-        this->data[this->front] = *t;
-        this->front % this->size + 1;
+        t = this->data[this->front];
+        this->front = this->front % this->size + 1;
     }
     return t;
 }
 
 bool Queue::is_empty() {
     if(this->front == this->rear) {
-        std::cout << "error, queue is empty" << std::endl;
+        printf("queue is empty\n\n");
         return true;
     }
     return false;
@@ -73,17 +68,10 @@ bool Queue::is_empty() {
 
 bool Queue::is_full() {
     if(this->rear % this->size + 1 == this->front) {
-        std::cout << "error, queue is full" << std::endl;
+        printf("queue is full\n\n");
         return true;
     }
     return false;
-}
-
-void Queue::display() {
-    for(int i=0; i<this->size; i++) {
-        std::cout << this->data[i].data << " " << std::endl;
-    }
-    std::cout << std::endl;
 }
 
 #endif

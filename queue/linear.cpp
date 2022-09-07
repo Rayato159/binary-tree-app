2860cb185f62c7aa0259a89b4251b9d9055802ea
Rayato159 <rayato159@gmail.com>
2022-09-07 23:48:51 +0700

    -feature :sparkles: :Linear queue was added

--- queue/linear.cpp
+++ queue/linear.cpp
@@ -3,26 +3,25 @@ using namespace std;
 
 class Queue {
     private:
-        int *Q;
+        int *q;
         int size;
         int front;
         int rear;
+
     public:
-        //Constructor
         Queue(){};
         Queue(int size);
 
-        //Methods
         void enqueue(int q);
         int dequeue();
         bool is_empty();
         bool is_full();
-        void swap(int *x, int *y);
         void display();
+        void swap(int *x, int *y);
 };
 
 Queue::Queue(int size) {
-    this->Q = new int[size]{0};
+    this->q = new int[size]{0};
     this->size = size;
     this->front = -1;
     this->rear = 0;
@@ -32,7 +31,7 @@ void Queue::enqueue(int q) {
     if(this->is_full()) {
         cout << "queue is full" << endl;
     } else {
-        this->Q[this->rear] = q;
+        this->q[this->rear] = q;
         this->rear++;
     }
 }
@@ -42,33 +41,33 @@ int Queue::dequeue() {
     if(this->is_empty()) {
         cout << "queue is empty" << endl;
     } else {
-        x = this->Q[0];
+        x = this->q[0]; // FIFO
         for(int i=0; i<this->rear; i++) {
-            swap(&this->Q[i+1], &this->Q[i]);
-            this->Q[i+1] = 0;
+            this->swap(&this->q[i+1], &this->q[i]);
+            this->q[i+1] = 0;
         }
         this->rear--;
     }
     return x;
 }
 
+bool Queue::is_full() {
+    return this->rear == this->size;
+}
+
+bool Queue::is_empty() {
+    return this->rear == 0;
+}
+
 void Queue::swap(int *x, int *y) {
     int temp = *x;
     *x = *y;
     *y = temp;
-} 
-
-bool Queue::is_empty() {
-    return this->front == this->rear - 1;
-}
-
-bool Queue::is_full() {
-    return this->front == this->rear;
 }
 
 void Queue::display() {
     for(int i=0; i<this->size; i++) {
-        cout << Q[i] << " " << flush; 
+        cout << this->q[i] << " " << flush;
     }
     cout << endl;
 }
@@ -80,6 +79,10 @@ int main() {
     q.enqueue(30);
     q.enqueue(40);
     q.enqueue(50);
+    q.display();
+    cout << q.dequeue() << endl;
+    cout << q.dequeue() << endl;
+    cout << q.dequeue() << endl;
     cout << q.dequeue() << endl;
     cout << q.dequeue() << endl;
     cout << q.dequeue() << endl;
